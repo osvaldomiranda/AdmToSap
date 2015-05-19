76f63f844633120f3ef673c29b55b97a7ed894f9
osvaldomiranda <osvaldo.omiranda@gmail.com>
2015-05-19 10:02:52 -0400

    Initial upload
    Se crea clase de conexión a los servicios Sap

---
+++ Connect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace AdmToSap
+{
+    class Connect
+    {
+
+
+        public  string HttpPOST(string url, string querystring) { 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers.Add("encoding: utf-8");
+            // or whatever - application/json, etc, etc 
+            Stream newStream = request.GetRequestStream();
+
+            StreamWriter requestWriter = new StreamWriter(newStream);
+            try { 
+                requestWriter.Write(querystring); 
+            } catch 
+            { 
+                throw; 
+            } 
+            finally
+            { 
+                requestWriter.Close(); 
+                requestWriter = null; 
+            }
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdmToSap
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string url = "http://192.168.100.43:8080" +
+                "/B1iXcellerator/exec/ipo/vP.0010000105.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc?"+
+                "wsaction="+
+                "GetPriceList";
+
+            string json = "hola";
+
+            Connect conn = new Connect();
+            String responce = conn.HttpPOST(url, json);
+
+            System.Console.WriteLine("LA RESPUESTA ES :" + responce);
+
+        }
+    }
+}
