8fa702b2db8b8e9fb9146a2a3cc9a1b1eb3b94d2
osvaldomiranda <osvaldo.omiranda@gmail.com>
2015-06-09 21:42:26 -0400

    [ADD] Class document

---
+++ Document.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace AdmToSap
+{
+    [DataContract]
+    class Document
+    {
+        [DataMember]
+        public String BoObjectType { get; set; }
+        [DataMember]
+        public String DocumentSubType { get; set; }
+        [DataMember]
+        public String CardCode { get; set; }
+        [DataMember]
+        public String DocDate { get; set; }
+        [DataMember]
+        public String DocDueDate { get; set; }
+        [DataMember]
+        public String TaxDate { get; set; }
+        [DataMember]
+        public String FolioNumber { get; set; }
+        [DataMember]
+        public String FolioPrefixString { get; set; }
+        [DataMember]
+        public String DiscountPercent { get; set; }
+        [DataMember]
+        public String Indicator { get; set; }
+
+        [DataMember]
+        public List<Item> items = new List<Item>();
+        [DataMember]
+        public List<Udf> udfs = new List<Udf>();
+    }
+
+    class Item
+    {
+        public String ItemCode { get; set; }
+        public String Quantity { get; set; }
+        public String UnitPrice { get; set; }
+        public String WarehouseCode { get; set; }
+        public String TaxCode { get; set; }
+        public String DiscountPercent { get; set; }
+    
+}
+
+    class Udf
+    {
+        public String U_SEI_FEBOSID { get; set; }
+        public String U_SEI_INREF { get; set; }
+        public String U_SEI_FOREF { get; set; }
+        public String U_SEI_FEREF { get; set; }
+        public String U_SEI_CREF { get; set; }
+
+    }
+}
